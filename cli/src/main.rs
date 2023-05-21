7ddf9604679a3be12ee6ee89290007dec7f9338a
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-05-21 02:01:35 -0300

    Add stats gatherer into cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,13 @@
 use std::{
     fs::{File, OpenOptions},
-    io::{self, BufReader, BufWriter},
+    io::{self, BufReader, BufWriter, Read, Write},
     path::{Path, PathBuf},
+    time::{Duration, Instant},
 };
 
 use clap::{Args, Parser, Subcommand, ValueEnum};
 use compressing::lzw;
+use stat::Stat;
 
 #[derive(Debug, Parser)]
 #[command(version)]
@@ -13,6 +15,10 @@ struct Cli {
     /// The algorithm to use for compress or decompress.
     #[arg(short, value_enum)]
     algorithm: Algorithm,
+
+    /// Whether the program should show statistics.
+    #[arg(long)]
+    stats: bool,
 
     #[command(subcommand)]
     action: Action,
@@ -25,12 +31,12 @@ enum Algorithm {
 
 #[derive(Debug, Subcommand)]
 enum Action {
-    Compress(ActionInput),
-    Decompress(ActionInput),
+    Compress(ActionData),
+    Decompress(ActionData),
 }
 
 #[derive(Debug, Args)]
-struct ActionInput {
+struct ActionData {
     /// The file to compress or decompress.
     input: PathBuf,
 
@@ -42,36 +48,89 @@ struct ActionInput {
 fn main() -> io::Result<()> {
     let cmd = Cli::parse();
 
-    match cmd.action {
-        Action::Compress(data) => {
-            let (mut reader, mut writer) = rw_pair(&data.input, &data.output)?;
-            match cmd.algorithm {
-                Algorithm::Lzw => {
-                    lzw::enc(&mut reader, &mut writer)?;
-                }
-            }
-            println!("done");
-        }
-        Action::Decompress(data) => {
-            let (mut reader, mut writer) = rw_pair(&data.input, &data.output)?;
-            match cmd.algorithm {
-                Algorithm::Lzw => {
-                    lzw::dec(&mut reader, &mut writer)?;
-                }
-            }
-            println!("done");
+    let data = cmd.action.data();
+    let manager = IoManager::new(&data.input, &data.output)?;
+
+    let stats = match cmd.action {
+        Action::Compress(_) => match cmd.algorithm {
+            Algorithm::Lzw => manager.run(lzw::enc)?,
+        },
+        Action::Decompress(_) => match cmd.algorithm {
+            Algorithm::Lzw => manager.run(lzw::dec)?,
+        },
+    };
+
+    if cmd.stats {
+        println!("done.");
+        println!("    in {} ms", stats.elapsed.as_millis());
+
+        if cmd.action.is_compress() {
+            // https://en.wikipedia.org/wiki/Data_compression_ratio
+            let space_saved = (1.0 - stats.written as f64 / stats.read as f64) * 100.0;
+            println!("    saved {space_saved:.2}%");
         }
     }
 
     Ok(())
 }
 
-fn rw_pair(input: &Path, output: &Path) -> io::Result<(BufReader<File>, BufWriter<File>)> {
-    let in_file = File::open(input)?;
-    let reader = BufReader::new(in_file);
+impl Action {
+    fn data(&self) -> &ActionData {
+        match self {
+            Action::Compress(data) => data,
+            Action::Decompress(data) => data,
+        }
+    }
 
-    let out_file = OpenOptions::new().create(true).write(true).open(output)?;
-    let writer = BufWriter::new(out_file);
+    fn is_compress(&self) -> bool {
+        matches!(self, Action::Compress(_))
+    }
+}
 
-    Ok((reader, writer))
+struct IoManager {
+    reader: BufReader<Stat<File>>,
+    writer: BufWriter<Stat<File>>,
+}
+
+impl IoManager {
+    /// Opens the given files and constructs a new [`IoManager`].
+    fn new(input: &Path, output: &Path) -> io::Result<Self> {
+        let reader = {
+            let file = File::open(input)?;
+            let stat = Stat::new(file);
+            BufReader::new(stat)
+        };
+        let writer = {
+            let file = OpenOptions::new().create(true).write(true).open(output)?;
+            let stat = Stat::new(file);
+            BufWriter::new(stat)
+        };
+        Ok(Self { reader, writer })
+    }
+
+    /// Runs the provided function and collects statistics on the involved I/O
+    /// operations.
+    fn run<F>(mut self, f: F) -> io::Result<Stats>
+    where
+        F: Fn(&mut dyn Read, &mut dyn Write) -> io::Result<()>,
+    {
+        let start = Instant::now();
+        f(&mut self.reader, &mut self.writer)?;
+        let elapsed = start.elapsed();
+
+        let stat_r = self.reader.into_inner();
+        let stat_w = self.writer.into_inner()?;
+
+        Ok(Stats {
+            read: stat_r.read_count(),
+            written: stat_w.write_count(),
+            elapsed,
+        })
+    }
+}
+
+struct Stats {
+    read: u64,
+    written: u64,
+    elapsed: Duration,
 }
